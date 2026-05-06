69ad2a03cf26050e92adef202fe0817fdbfb52d8
Vardaan <vardaan.bijnor@gmail.com>
2026-05-06 20:40:15 +0530

    added delete task command

--- app.cpp
+++ app.cpp
@@ -10,20 +10,44 @@ public:
     std::string updatedAt;
 };
 
-void add(std::string input, int index, std::vector<Task>& list){
+int add(std::string input, int index, std::vector<Task>& list){
     std::string desc;
     std::string status;
     std::string rn;
+    int size = list.size();
     time_t timestamp;
+
     time(&timestamp);
     rn = ctime(&timestamp);
+
     desc = input.substr(index + 1);
-    int size = list.size();
+    if (desc.empty()){
+        std::cout << "No task given";
+        return 0;
+    }
+
     std::cout << "There are 3 status : \n1.to-do \n2. in-progress \n3. done \n\nGive a status to your task \n>>>> ";
     std::cin >> status;
     std::cin.ignore(10000, '\n');
+
     Task task{(size + 1), desc, rn, rn};
+    std::cout << task.id;
     list.push_back(task);
+    return 0;
+}
+
+void del(std::string input, int index, std::vector<Task>& list){
+    int id;
+    int size = list.size();
+
+    id = std::stoi(input.substr(index + 1));
+
+    for(int i = 0; i < size; i++){
+        if(id == (list[i].id)){
+            list.erase(list.begin() + i);
+            break;
+        }
+    }
 }
 
 int main(){
@@ -33,7 +57,7 @@ int main(){
     std::string command;
     std::string input;
     int command_index;
-    std::string commands[] = {"add"};
+    std::string commands[] = {"add", "exit", "delete"};
     int amt_of_commands = sizeof(commands)/sizeof(commands[0]);
     int index;
 
@@ -54,12 +78,18 @@ int main(){
             case 0:
             add(input, index, list);
             break;
+            case 1:
+            running = false;
+            break;
+            case 2:
+            del(input, index, list);
+            break;
             default:
             std::cout << "Invalid Command\n";
         }
     }
 
-    std::cout << "*****************************************";
+    std::cout << "****************************";
 
     return 0;
 }
